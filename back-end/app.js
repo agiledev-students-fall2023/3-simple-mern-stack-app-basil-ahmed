9bcd23845a5e50d1d8a183a7559fc30333715511
basil-ahmed <bq2024@nyu.edu>
2023-09-30 19:33:43 -0400

    Completed Assignment

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,7 @@ const express = require('express') // CommonJS import style!
 const morgan = require('morgan') // middleware for nice logging of incoming HTTP requests
 const cors = require('cors') // middleware for enabling CORS (Cross-Origin Resource Sharing) requests.
 const mongoose = require('mongoose')
+const fs = require('fs');
 
 const app = express() // instantiate an Express object
 app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' })) // log all incoming requests, except when in unit test mode.  morgan has a few logging default styles - dev is a nice concise color-coded style
@@ -77,6 +78,25 @@ app.post('/messages/save', async (req, res) => {
     })
   }
 })
+
+app.get('/info', (req, res) => {
+  try {
+    const aboutUsData = {
+      paragraphs: [
+        "Welcome to our MERN stack app! I am Basil Ahmed Qureshi, a passionate and driven individual with a diverse range of skills and experiences. Currently pursuing a Bachelor of Science degree in Computer Science, Interactive Media, and Applied Mathematics at New York University Abu Dhabi, I have had the privilege of being awarded a Full-Ride Scholarship, a testament to my dedication to academics.",
+        "My journey in the field of computer science and technology has been enriched by various roles and internships. At eBrain Lab, I served as a Machine Learning Research Assistant, where I collaborated with PostDocs on embedded ML projects, including design, implementation, and testing. I also delved into the world of cybersecurity by developing and implementing adversarial attacks on the MNIST dataset. Benchmarking, MLOps, and prototyping autonomous system algorithms were part of my daily endeavors.",
+        "In the realm of personal projects, I have showcased my coding prowess with projects like Kid Going Downstairs, Personal Portfolio, Come With, People Counter, and Flight Ticket Management System, each demonstrating my proficiency in various programming languages and web development technologies.",
+        "My skill set includes programming languages such as Python, C++, JavaScript, and proficiency in tools like Unity, Blender, PyTorch, and the NVIDIA Nano Developer's Kit. I am well-versed in operating systems such as Windows, Linux, and Mac. Furthermore, my web development expertise encompasses HTML, CSS, JavaScript, Node.js, Sockets.io, and MongoDB. I also possess strong analytical, problem-solving, organizational, and communication skills, which have been invaluable throughout my academic and professional journey.",
+        "As I look forward to my expected graduation in May 2025, I am eager to continue pushing the boundaries of technology and innovation, contributing to the ever-evolving world of computer science, interactive media, and mathematics."
+      ],
+      imageUrl: 'https://avatars.githubusercontent.com/u/90772853?s=400&u=2320a2057075406233f0a111947b0a411b73e906&v=4',
+    };
+    res.json(aboutUsData);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 // export the express app we created to make it available to other modules
 module.exports = app // CommonJS export style!
--- front-end/src/About.js
+++ front-end/src/About.js
@@ -1,56 +1,43 @@
-import React from 'react';
-import './About.css'; // You can import your CSS file here.
-import aboutImage from './basil.png'; // Import your image.
+import axios from 'axios';
+import React, { Component } from 'react';
 
-function AboutUs() {
-  return (
-    <div className="about-us">
-      <h1>About Us</h1>
+class AboutUs extends Component {
+  state = {
+    paragraphs: [],
+    imageUrl: '',
+  };
 
-        {<br></br>}
+  componentDidMount() {
+    // Fetch data from the backend API endpoint
+    fetch(`${process.env.REACT_APP_SERVER_HOSTNAME}/info`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          paragraphs: data.paragraphs,
+          imageUrl: data.imageUrl,
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }
 
-        {<p>
-        Welcome to our MERN stack app! I am Basil Ahmed Qureshi, 
-        a passionate and driven individual with a diverse range of skills and experiences. 
-        Currently pursuing a Bachelor of Science degree in Computer Science, Interactive Media, and Applied Mathematics at New York University Abu Dhabi, 
-        I have had the privilege of being awarded a Full-Ride Scholarship, a testament to my dedication to academics.</p>}
-        
-        {<br></br>}
-
-        {<p>
-        My journey in the field of computer science and technology has been enriched by various roles and internships. 
-        At eBrain Lab, I served as a Machine Learning Research Assistant, where I collaborated with PostDocs on embedded ML projects, 
-        including design, implementation, and testing. 
-        I also delved into the world of cybersecurity by developing and implementing adversarial attacks on the MNIST dataset. 
-        Benchmarking, MLOps, and prototyping autonomous system algorithms were part of my daily endeavors.</p>}
-        
-        {<br></br>}
-
-        {<p>
-        In the realm of personal projects, I have showcased my coding prowess with projects like "Kid's Going Downstairs," "Personal Portfolio," "Come With," "People Counter," and "Flight Ticket Management System," 
-        each demonstrating my proficiency in various programming languages and web development technologies.</p>}
-        
-        {<br></br>}
-
-        {<p>
-        My skill set includes programming languages such as Python, C++, JavaScript, and proficiency in tools like Unity, Blender, PyTorch, and the NVIDIA Nano Developer's Kit. 
-        I am well-versed in operating systems such as Windows, Linux, and Mac. 
-        Furthermore, my web development expertise encompasses HTML, CSS, JavaScript, Node.js, Sockets.io, and MongoDB. 
-        I also possess strong analytical, problem-solving, organizational, and communication skills, which have been invaluable throughout my academic and professional journey.</p>}
-
-        {<br></br>}
-
-        {<p>
-        As I look forward to my expected graduation in May 2025, I am eager to continue pushing the boundaries of technology and innovation, contributing to the ever-evolving world of computer science, interactive media, and mathematics.
-        </p>}
-
-        {<br></br>}
-
-        {/* Display my image here */}
-        {<img src={aboutImage} alt="Basil Ahmed Qureshi" width="400" height="400"></img>}
-    
-    </div>
-  );
+  render() {
+    return (
+      <div className="about-us">
+        <h1>About Us</h1>
+        {this.state.paragraphs.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p> 
+        ))}
+        <img src={this.state.imageUrl} alt="About Us" />
+      </div>
+    );
+  }
 }
 
 export default AboutUs;
